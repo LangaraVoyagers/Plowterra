9641cf467a8a7d1fb2bed9beb9ecfca67a899656
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-02-17 16:21:32 -0800

    feat(pickers): Create pickers layout

--- src/pages/pickers/Pickers.tsx
+++ src/pages/pickers/Pickers.tsx
@@ -1,6 +1,109 @@
-import { Box, Button } from "@mui/material";
-
+import { Search } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import BasicHome from "layouts/BasicHome";
+
+const columns: GridColDef[] = [
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+    flex: 1,
+    editable: true,
+  },
+  {
+    field: "phone",
+    headerName: "Phone Number",
+    width: 150,
+  },
+  {
+    field: "contactName",
+    headerName: "Emergency Contact",
+    width: 200,
+    valueGetter: (params: GridValueGetterParams) => params.row.contacts.name,
+  },
+  {
+    field: "contactPhone",
+    headerName: "",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) => params.row.contacts.phone,
+  },
+
+  {
+    field: "action",
+    headerName: "",
+    width: 150,
+    renderCell: () => {
+      return <Button>View More</Button>;
+    },
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    name: "Snow",
+    phone: "123 456 447",
+    contacts: {
+      name: "Jon",
+      phone: 14,
+    },
+  },
+  {
+    id: 2,
+    name: "Lannister",
+    phone: "123 456 447",
+    contacts: {
+      name: "Cersei",
+      phone: 31,
+    },
+  },
+  {
+    id: 3,
+    name: "Lannister",
+    phone: "123 456 447",
+    contacts: {
+      name: "Jaime",
+      phone: 31,
+    },
+  },
+  {
+    id: 4,
+    name: "Stark",
+    phone: "123 456 447",
+    contacts: {
+      name: "Arya",
+      phone: 11,
+    },
+  },
+  {
+    id: 5,
+    name: "Targaryen",
+    phone: "123 456 447",
+    contacts: {
+      name: "Daenerys",
+      phone: null,
+    },
+  },
+  {
+    id: 6,
+    name: "Melisandre",
+    phone: "123 456 447",
+    contacts: {
+      name: null,
+      phone: 150,
+    },
+  },
+];
 
 const Pickers = () => {
   return (
@@ -13,7 +116,53 @@ const Pickers = () => {
       ]}
       actions={<Button variant="contained">Add New Picker</Button>}
     >
-      <Box></Box>
+      <Box display="flex" justifyContent="space-between">
+        <FormControl>
+          <InputLabel id="sortby-label">Sort</InputLabel>
+
+          <Select
+            labelId="sortby-label"
+            id="sortby-select"
+            value={1}
+            label="Sort:"
+            size="small"
+          >
+            <MenuItem value={1}>Latest to Oldest</MenuItem>
+            <MenuItem value={2}>Oldest to Latest</MenuItem>
+            <MenuItem value={3}>A to Z</MenuItem>
+            <MenuItem value={4}>Z to A</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            placeholder="Search"
+            size="small"
+            startAdornment={
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Box>
+
+      <Box>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          getRowId={(data) => data.id}
+          pageSizeOptions={[5, 10]}
+          disableRowSelectionOnClick
+        />
+      </Box>
     </BasicHome>
   );
 };
