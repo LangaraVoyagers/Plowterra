42472637c5bff5ddafe1a5cf494f1a42d45dad32
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-02-27 09:49:33 -0800

    style: customize the font size a bit

--- src/theme.tsx
+++ src/theme.tsx
@@ -13,6 +13,35 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  typography: {
+    allVariants: {
+      // fontFamily: "Plus Jakarta Sans",
+    },
+    h1: {
+      fontSize: "2.75rem",
+      lineHeight: "3.5rem",
+      letterSpacing: "-0.033rem",
+    },
+    h2: {
+      fontSize: "2rem",
+      lineHeight: "3.5rem",
+      letterSpacing: "-0.033rem",
+    },
+    body1: {
+      fontSize: "1rem",
+      lineHeight: " 1.5rem",
+    },
+    overline: {
+      fontSize: "0.75rem",
+      lineHeight: "1.125rem",
+      letterSpacing: "0.0225rem",
+    },
+    // Disable variants
+    h3: undefined,
+    h4: undefined,
+    h5: undefined,
+    h6: undefined,
+  },
 });
 
 export default theme;
