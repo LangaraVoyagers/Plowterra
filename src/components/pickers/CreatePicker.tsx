58cf829b9d5366f98fa5066c3573f4e1d65ee46e
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-02-28 17:54:27 -0800

    debug: testing github actions

--- src/components/pickers/CreatePicker.tsx
+++ src/components/pickers/CreatePicker.tsx
@@ -17,6 +17,10 @@ const CreatePicker = () => {
           id="pickers.create.button.text"
           defaultMessage="Add New Picker"
         />
+        <FormattedMessage
+          id="pickers.create.button.test"
+          defaultMessage="Test"
+        />
       </Button>
 
       <PickerDrawer open={open} dismiss={hideDrawer} />
