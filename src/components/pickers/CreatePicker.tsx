c86ebed4f5b1583804b16b88e5b7aab136db7602
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-03-07 08:16:35 -0800

    fix: button padding

--- src/components/pickers/CreatePicker.tsx
+++ src/components/pickers/CreatePicker.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 import PickerDrawer from "./PickerDrawer";
 import { FormattedMessage } from "react-intl";
+import { UserPlus } from "@phosphor-icons/react";
 
 const CreatePicker = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -12,7 +13,11 @@ const CreatePicker = () => {
 
   return (
     <div>
-      <Button variant="contained" onClick={showDrawer}>
+      <Button
+        variant="contained"
+        onClick={showDrawer}
+        endIcon={<UserPlus size={20} />}
+      >
         <FormattedMessage
           id="pickers.create.button.text"
           defaultMessage="Add New Picker"
