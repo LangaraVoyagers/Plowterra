d3c4f455f84772be8384a9ccd9f72d91c0847665
Reinhardt Botha <botha.wr@gmail.com>
2024-03-14 22:16:01 -0700

    feat: add intl

--- src/pages/Seasons/Seasons.tsx
+++ src/pages/Seasons/Seasons.tsx
@@ -1,8 +1,12 @@
 import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
 
 import BasicHome from "layouts/BasicHome";
-import formatDate from "shared/formatDate";
 import { getSeasons } from "api/seasons";
 import { useQuery } from "react-query";
 import useQueryCache from "hooks/useQueryCache";
@@ -11,41 +15,87 @@ import { useUser } from "context/UserProvider";
 import CreateSeason from "components/seasons/CreateSeason";
 import UpdateSeason from "components/seasons/UpdateSeason";
 import SearchDataGrid from "components/SearchDataGrid";
+import { FormattedDate, FormattedMessage, useIntl } from "react-intl";
+import { ISeason } from "project-2-types";
 
 const statuses = ["Active", "Closed"];
 
 const columns: GridColDef[] = [
   {
     field: "name",
-    headerName: "Name",
+    renderHeader: () => (
+      <FormattedMessage id="seasons.table.column.name" defaultMessage="Name" />
+    ),
     width: 250,
-    flex: 1,
     minWidth: 250,
   },
   {
     field: "status",
-    headerName: "Status",
+    renderHeader: () => (
+      <FormattedMessage
+        id="seasons.table.column.status"
+        defaultMessage="Status"
+      />
+    ),
     width: 200,
   },
   {
     field: "product",
-    headerName: "Product",
+    renderHeader: () => (
+      <FormattedMessage
+        id="seasons.table.column.product"
+        defaultMessage="Product"
+      />
+    ),
     width: 200,
     valueGetter: (params) => params.row.product?.name,
   },
   {
     field: "startDate",
-    headerName: "Start Date",
+    renderHeader: () => (
+      <FormattedMessage
+        id="seasons.table.column.start_date"
+        defaultMessage="Start Date"
+      />
+    ),
     width: 150,
-    valueFormatter: (params) =>
-      params?.value ? formatDate(params.value) : "-",
+    valueGetter: (params: GridValueGetterParams<ISeason>) =>
+      params.row.startDate,
+    renderCell: (params: GridRenderCellParams<ISeason>) => {
+      return (
+        <FormattedDate
+          value={params.row.startDate}
+          year="numeric"
+          month="long"
+          day="numeric"
+        />
+      );
+    },
   },
   {
     field: "endDate",
-    headerName: "End Date",
+    renderHeader: () => (
+      <FormattedMessage
+        id="seasons.table.column.end_date"
+        defaultMessage="End Date"
+      />
+    ),
     width: 150,
-    valueFormatter: (params) =>
-      params?.value ? formatDate(params.value) : "-",
+    // valueFormatter: (params) =>
+    //   params?.value ? formatDate(params.value) : "-",
+    valueGetter: (params: GridValueGetterParams<ISeason>) => params.row.endDate,
+    renderCell: (params: GridRenderCellParams<ISeason>) => {
+      if (!params?.row.endDate) return "-";
+      else
+        return (
+          <FormattedDate
+            value={params.row.endDate}
+            year="numeric"
+            month="long"
+            day="numeric"
+          />
+        );
+    },
   },
   {
     field: "action",
@@ -60,6 +110,7 @@ const columns: GridColDef[] = [
 
 const Seasons = () => {
   const { user } = useUser();
+  const intl = useIntl();
 
   const { GET_QUERY_KEY } = useQueryCache("seasons");
   const [seasons, setSeasons] = useState([]);
@@ -94,11 +145,25 @@ const Seasons = () => {
 
   return (
     <BasicHome
-      title="Harvest Season"
-      subtitle="Create and close your harvest season here."
+      title={intl.formatMessage({
+        id: "harvestSeason",
+        defaultMessage: "Harvest Season",
+      })}
+      subtitle={intl.formatMessage({
+        id: "harvestSeason.subtitle",
+        defaultMessage: "Create and close your harvest season here.",
+      })}
       breadcrumb={[
         { title: user.farm.name, href: "#" },
-        { title: "Harvest Seasons", href: "" },
+        {
+          title: (
+            <FormattedMessage
+              id="sidebar.harvestSeason"
+              defaultMessage="Harvest Season"
+            />
+          ),
+          href: "",
+        },
       ]}
       actions={<CreateSeason />}
     >
