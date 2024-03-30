47df9cc1d0fb176ca50492de2f7ea96a2a0e5675
ALFAROSO <alberth.sotelo@gmail.com>
2024-03-30 12:12:55 -0700

    feat: pickers.sort.az

--- src/pages/Pickers/Pickers.tsx
+++ src/pages/Pickers/Pickers.tsx
@@ -209,7 +209,7 @@ const Pickers = () => {
           sortModel={sortModel[0]}
           setSortModel={setSortModel}
           options={[
-            { field: "name", sort: "asc", label: "A to Z" },
+            { field: "name", sort: "asc", label: intl.formatMessage({ id: "pickers.sort.az", defaultMessage: "A to Z" }) },
             { field: "name", sort: "desc", label: "Z to A" },
             { field: "createdAt", sort: "desc", label: intl.formatMessage({ id: "pickers.sort.recent", defaultMessage: "Recent" }) },
             { field: "createdAt", sort: "asc", label: intl.formatMessage({ id: "pickers.sort.long", defaultMessage: "Long-standing" }) },
